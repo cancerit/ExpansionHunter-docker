d107170e702abcc69116d045502ca23a86b8a909
Raul Alcantara <ra11@sanger.ac.uk>
2022-10-20 14:14:20 +0000

    working Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-FROM  ubuntu:22.04 as builder
+FROM  ubuntu:20.04 as builder
 
 USER  root
 
 # ALL tool versions used by opt-build.sh
-ENV VER_LIBDEFLATE="v1.12"
-ENV VER_KOURAMI="v0.9.6"
-ENV VER_SAMTOOLS="1.14"
-ENV VER_HTSLIB="1.14"
-ENV VER_BAMUTIL="v1.0.15"
-ENV VER_BWA="v0.7.17"
+ENV VER_EXPANSIONHUNTER="v5.0.0"
 
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get -yq update
@@ -17,8 +12,8 @@ RUN apt-get install -yq --no-install-recommends ca-certificates
 RUN apt-get install -yq --no-install-recommends pkg-config
 RUN apt-get install -yq --no-install-recommends wget
 RUN apt-get install -yq --no-install-recommends locales
-RUN apt-get install -yq --no-install-recommends maven
 RUN apt-get install -yq --no-install-recommends make
+RUN apt-get install -yq --no-install-recommends cmake
 RUN apt-get install -yq --no-install-recommends bzip2
 RUN apt-get install -yq --no-install-recommends gcc
 RUN apt-get install -yq --no-install-recommends g++
@@ -26,8 +21,9 @@ RUN apt-get install -yq --no-install-recommends zlib1g-dev
 RUN apt-get install -yq --no-install-recommends libbz2-dev
 RUN apt-get install -yq --no-install-recommends liblzma-dev
 RUN apt-get install -yq --no-install-recommends libcurl4-openssl-dev
-RUN apt-get install -yq --no-install-recommends libncurses5-dev
 RUN apt-get install -yq --no-install-recommends libssl-dev
+RUN apt-get install -yq --no-install-recommends git
+
 
 RUN locale-gen en_US.UTF-8
 RUN update-locale LANG=en_US.UTF-8
@@ -45,16 +41,15 @@ ADD build/opt-build.sh build/
 RUN bash build/opt-build.sh $OPT
 
 
-
 # build the tools in this repo, separate to reduce build time on errors
 COPY . .
 
-FROM ubuntu:22.04
+FROM ubuntu:20.04
 
 LABEL maintainer="cgphelp@sanger.ac.uk" \
       uk.ac.sanger.cgp="Cancer, Ageing and Somatic Mutation, Wellcome Trust Sanger Institute" \
       version="1.0.0" \
-      description="kourami docker"
+      description="ExpansionHunter docker"
 
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get -yq update
@@ -64,7 +59,6 @@ locales \
 curl \
 ca-certificates \
 time \
-default-jre \
 unattended-upgrades && \
 unattended-upgrade -d -v && \
 apt-get remove -yq unattended-upgrades && \
--- build/opt-build.sh
+++ build/opt-build.sh
@@ -56,7 +56,7 @@ set -e
 mkdir -p $INST_PATH
 cd $INST_PATH
 INST_PATH=`pwd`
-mkdir -p $INST_PATH/bin
+mkdir -p $INST_PATH
 cd $INIT_DIR
 
 export PATH="$INST_PATH/bin:$PATH"
@@ -65,117 +65,23 @@ export PATH="$INST_PATH/bin:$PATH"
 SETUP_DIR=$INIT_DIR/install_tmp
 mkdir -p $SETUP_DIR
 
-echo -n "Building libdeflate ..."
-if [ -e $SETUP_DIR/libdeflate.success ]; then
+echo -n "Building ExpansionHunter ..."
+if [ -e $SETUP_DIR/ehunter.success ]; then
   echo " previously built ...";
 else
   echo
   cd $SETUP_DIR
-  mkdir -p libdeflate
-  get_distro "libdeflate" "https://github.com/ebiggers/libdeflate/archive/$VER_LIBDEFLATE.tar.gz"
-  tar --strip-components 1 -C libdeflate -zxf libdeflate.tar.gz
-  cd libdeflate
-  make -j$CPU CFLAGS="-fPIC -O3" libdeflate.a
-  PREFIX=$INST_PATH make install
-  cd $SETUP_DIR
-  rm -r libdeflate.tar.gz
-  touch $SETUP_DIR/libdeflate.success
-fi
-
-echo -n "Building htslib ..."
-if [ -e $SETUP_DIR/htslib.success ]; then
-  echo " previously built ...";
-else
-  echo
-  cd $SETUP_DIR
-  mkdir -p htslib
-  get_distro "htslib" "https://github.com/samtools/htslib/releases/download/$VER_HTSLIB/htslib-$VER_HTSLIB.tar.bz2"
-  tar --strip-components 1 -C htslib -jxf htslib.tar.bz2
-  cd htslib
-  export CFLAGS="-I$INST_PATH/include"
-  export LDFLAGS="-L$INST_PATH/lib"
-  ./configure --disable-plugins  --enable-libcurl --with-libdeflate --prefix=$INST_PATH
-  make -j$CPU
-  make install
-  cd $SETUP_DIR
-  rm -r htslib.tar.bz2
-  unset CFLAGS
-  unset LDFLAGS
-  touch $SETUP_DIR/htslib.success
-fi
-
-echo -n "Building samtools ..."
-if [ -e $SETUP_DIR/samtools.success ]; then
-  echo " previously built ...";
-else
-  echo
-  cd $SETUP_DIR
-  rm -rf samtools
-  get_distro "samtools" "https://github.com/samtools/samtools/releases/download/$VER_SAMTOOLS/samtools-$VER_SAMTOOLS.tar.bz2"
-  mkdir -p samtools
-  tar --strip-components 1 -C samtools -xjf samtools.tar.bz2
-  cd samtools
-  ./configure --with-htslib=$SETUP_DIR/htslib --enable-plugins --enable-libcurl --prefix=$INST_PATH
-  make -j$CPU
-  make install
-  cd $SETUP_DIR
-  rm -f samtools.tar.bz2
-  touch $SETUP_DIR/samtools.success
-fi
-
-echo -n "Building kourami ..."
-if [ -e $SETUP_DIR/kourami.success ]; then
-  echo " previously built ...";
-else
-  echo
-  cd $SETUP_DIR
-  rm -rf kourami
-  get_distro "kourami" "https://github.com/Kingsford-Group/kourami/archive/refs/tags/${VER_KOURAMI}.tar.gz"
-  mkdir -p kourami
-  tar --strip-components 1 -C kourami -xzf kourami.tar.gz
-  cd kourami
-  mvn install
-  cp -r target $INST_PATH/java
-  cp scripts/* $INST_PATH/bin
-  cd $SETUP_DIR
-  rm -f kourami.tar.gz
-  touch $SETUP_DIR/kourami.success
-fi
-
-echo -n "Building BamUtil ..."
-if [ -e $SETUP_DIR/bamutil.success ]; then
-  echo " previously built ...";
-else
-  echo
-  cd $SETUP_DIR
-  rm -rf bamutil
-  get_distro "bamutil" "https://github.com/statgen/bamUtil/archive/refs/tags/${VER_BAMUTIL}.tar.gz"
-  mkdir -p bamutil
-  tar --strip-components 1 -C bamutil -xzf bamutil.tar.gz
-  mkdir libStatGen
-  get_distro "libStatGen" "https://github.com/statgen/libStatGen/archive/refs/tags/${VER_BAMUTIL}.tar.gz"
-  tar --strip-components 1 -C libStatGen -xzf libStatGen.tar.gz
-  cd bamutil
-  make install INSTALLDIR=$INST_PATH/bin
-  cd $SETUP_DIR
-  rm -f kamutil.tar.gz
-  touch $SETUP_DIR/bamutil.success
-fi
-
-echo -n "Building bwa ..."
-if [ -e $SETUP_DIR/bwa.success ]; then
-  echo " previously built ...";
-else
-  echo
-  cd $SETUP_DIR
-  rm -rf bwa 
-  get_distro "bwa" "https://github.com/lh3/bwa/archive/refs/tags/${VER_BWA}.tar.gz"
-  mkdir -p bwa
-  tar --strip-components 1 -C bwa -xzf bwa.tar.gz
-  cd bwa
-  sed -i '/const uint8_t rle_auxtab\[8\];/d' ./rle.h #need to remove this for gcc10
+  mkdir -p ehunter
+  get_distro "ehunter" "https://github.com/Illumina/ExpansionHunter/archive/refs/tags/${VER_EXPANSIONHUNTER}.tar.gz"
+  tar --strip-components 1 -C ehunter -zxf ehunter.tar.gz
+  cd ehunter
+  mkdir build
+  cd build
+  cmake ..
   make
-  mv bwa $INST_PATH/bin
-  touch $SETUP_DIR/bwa.success
+  mv install/* $INST_PATH
+  cd $SETUP_DIR
+  rm -r ehunter.tar.gz
+  touch $SETUP_DIR/ehunter.success
 fi
 
